a869251b6810c27cc15416346c7f3746865a683a
U425651 <daedalus.chan@wellsfargo.com>
2016-03-17 17:08:40 +0800

    Print poll content to stdout as well

--- hk_gathering.py
+++ hk_gathering.py
@@ -63,6 +63,7 @@ class HKGathering(telepot.helper.ChatHandler):
         if flavor == 'normal':
             content_type, chat_type, _chat_id = telepot.glance2(msg)
             print('Normal Message:', content_type, chat_type, _chat_id, '; message content: ', msg)
+            print('Poll:' + self._poll.__str__()）
 
             if self._converType == ConverType.nothing:
 
