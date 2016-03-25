fb591200cbcc82a8c7ee92dc9942a9dcaa7fae47
Daedalus Chan <daedalus.chan@gmail.com>
2016-03-26 00:02:38 +0800

    group init chat offline with individual

--- hk_gathering.py
+++ hk_gathering.py
@@ -232,8 +232,8 @@ class HKGathering(telepot.helper.ChatHandler):
                     elif msg['text'].encode(encoding='utf-8') == '開始' or msg['text'].startswith('/begin'):
                         found_poll = 0
                         for poll_key in allPoll.keys():
-                            for each_resp in allPoll[poll_key].response:
-                                print(each_resp)
+                            for each_resp in (allPoll[poll_key].response):
+                                print('each_resp: ' + each_resp)
                                 if each_resp.userid == msg['from']['id']:
                                     found_poll = poll_key
                         if found_poll != 0:
