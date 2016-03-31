453f656d90b2f06e5bdbe9cec6985ee98f82f8a5
U425651 <daedalus.chan@wellsfargo.com>
2016-03-31 09:35:33 +0800

    add debug line to debug

--- hk_gathering.py
+++ hk_gathering.py
@@ -248,8 +248,9 @@ class HKGathering(telepot.helper.ChatHandler):
         show_keyboard = {'keyboard': [['開始回應']]}
         print('DEBUG: sendMessage: ' + show_keyboard.__str__())
         print('DEBUG: gen_survey_str: ' + self._poll.gen_survey_str())
-        print('DEBUG: whole text: ' + self._poll.gen_survey_str().encode(encoding='utf-8'))
-        self.sender.sendMessage(text=self._poll.gen_survey_str().encode(encoding='utf-8') + '\n' +
+        print('DEBUG: whole text: ' + self._poll.gen_survey_str() + '\n' +
+                                     '請用 ')
+        self.sender.sendMessage(text=self._poll.gen_survey_str() + '\n' +
                                      '請用 ' + start_link.encode(encoding='utf-8') +
                                      ' ﹝如第一次用' + botName.encode(encoding='utf8') + '﹞或者用 '+
                                      ans_link +  ' 回應問題。\n' +
