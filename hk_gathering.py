ed4f13f2de32411eb3156322ad9b3c0e0432921f
U425651 <daedalus.chan@wellsfargo.com>
2016-04-04 14:42:57 +0800

    add pref

--- hk_gathering.py
+++ hk_gathering.py
@@ -141,7 +141,7 @@ class Poll():
                 choice_str = chkNConv(choice_str) + '- ' + chkNConv(choice) + '\n'
             else:
                 choice_str = chkNConv(choice_str) + '- /' + i.__str__() + ' ' + chkNConv(choice) + \
-                u' -- 現在回應：'
+                u' -- 你答：'
                 if response_attached[i-1]:
                     choice_str = chkNConv(choice_str) + u'Yes\n'
                 else:
@@ -263,10 +263,7 @@ class HKGathering(telepot.helper.ChatHandler):
     def start_survey(self, poll_id, userid):
         print('start survey with poll id: ' + poll_id)
 
-        print('DEBUG: construct keyboard')
         show_keyboard = self._poll.genResponseKeyboard(allPoll[poll_id].response[userid.__str__()].preference)
-        print('DEBUG: sendMessage')
-        print('DEBUG: gen_survey_str: ' + self._poll.gen_survey_str(response_attached=allPoll[poll_id].response[userid.__str__()].preference).encode(encoding='utf-8'))
         self.sender.sendMessage(text=self._poll.gen_survey_str(response_attached=allPoll[poll_id].response[userid.__str__()].preference).encode(encoding='utf-8') +
                                      '\n你可以用 / 〈數字〉更改你對相關選項的回應，用 pop up 鍵盤亦可。\n' +
                                      '或者用 /add_pref 加入新選項。\n\n' +
